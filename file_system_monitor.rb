a41f9b532192868eeedd717fbfefc2189845dfdd
Max Bileschi <mbileschi@twitter.com>
2015-11-03 12:16:01 -0500

    update rb-fsevent dependency to use mlbileschi tap

--- file_system_monitor.rb
+++ file_system_monitor.rb
@@ -4,7 +4,7 @@ class FileSystemMonitor < Formula
   version "1.0.0"
   sha256 "d0834861463b9e819a009bb7c44ecaff72ee687a1715fd7e7d383ae6faa4d026"
 
-  depends_on "rb-fsevent"
+  depends_on "mlbileschi/tap/rb-fsevent"
 
   def install
     bin.install "file_system_monitor.rb"
