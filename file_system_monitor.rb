9eba1f48c1dfdb020f1eae88a12b71fab8b73417
Max Bileschi <mbileschi@twitter.com>
2015-11-03 13:05:46 -0500

    Update to 1.0.1

--- file_system_monitor.rb
+++ file_system_monitor.rb
@@ -1,8 +1,7 @@
 class FileSystemMonitor < Formula
   homepage "https://github.com/mlbileschi/file_system_monitor"
-  url "https://github.com/mlbileschi/file_system_monitor/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "d0834861463b9e819a009bb7c44ecaff72ee687a1715fd7e7d383ae6faa4d026"
+  url "https://github.com/mlbileschi/file_system_monitor/archive/1.0.1.tar.gz"
+  sha256 "91aba0a02212f0d1e64daec0f0a5b847c0547d6a"
 
   def install
     system "gem", "install", "rb-fsevent", "--version", "0.9.4", "--install-dir", prefix
