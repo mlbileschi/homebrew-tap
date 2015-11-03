3909bf31bc72a015823ac74f69b9d44329e6e15f
Max Bileschi <mbileschi@twitter.com>
2015-11-03 12:18:43 -0500

    Update to manually pull in rb-fsevent and not depend on the other formula

--- file_system_monitor.rb
+++ file_system_monitor.rb
@@ -4,9 +4,8 @@ class FileSystemMonitor < Formula
   version "1.0.0"
   sha256 "d0834861463b9e819a009bb7c44ecaff72ee687a1715fd7e7d383ae6faa4d026"
 
-  depends_on "mlbileschi/tap/rb-fsevent"
-
   def install
+    system "gem", "install", "rb-fsevent", "--version", "0.9.4", "--install-dir", prefix
     bin.install "file_system_monitor.rb"
   end
 end
