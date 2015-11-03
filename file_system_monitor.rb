4b89a6ec7abbbf30e4752c8236df784f24c40557
Max Bileschi <mbileschi@twitter.com>
2015-11-03 13:37:58 -0500

    Update 1.0.2

--- file_system_monitor.rb
+++ file_system_monitor.rb
@@ -1,6 +1,6 @@
 class FileSystemMonitor < Formula
   homepage "https://github.com/mlbileschi/file_system_monitor"
-  url "https://github.com/mlbileschi/file_system_monitor/archive/1.0.1.tar.gz"
+  url "https://github.com/mlbileschi/file_system_monitor/archive/1.0.2.tar.gz"
   sha256 "cf0d61b7a2b293a70b88d47d96a1d53c6daa06e4c00f46440b21e35b9f97e9e4"
 
   def install
