d268365c4bed937489cd7090cb865604da5499da
Max Bileschi <mbileschi@twitter.com>
2015-11-03 13:38:40 -0500

    Update sha to be correct

--- file_system_monitor.rb
+++ file_system_monitor.rb
@@ -1,7 +1,7 @@
 class FileSystemMonitor < Formula
   homepage "https://github.com/mlbileschi/file_system_monitor"
   url "https://github.com/mlbileschi/file_system_monitor/archive/1.0.2.tar.gz"
-  sha256 "cf0d61b7a2b293a70b88d47d96a1d53c6daa06e4c00f46440b21e35b9f97e9e4"
+  sha256 "b94f5c45d8e78fe2602764343f144315d8a96dab962e90ebf5475c09804834e8"
 
   def install
     system "gem", "install", "rb-fsevent", "--version", "0.9.4", "--install-dir", prefix
